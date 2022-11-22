ecfa2727947cef4950e9e8b39608e74acc5bf126
Shiori <shiori0921syd@gmail.com>
2022-11-22 16:35:19 +1100

    Create screen reg/home

---
+++ config/Config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+	apiKey: "AIzaSyCV4i23xWkn4dJS3TPIB4hLj7VLko9lgwg",
+	authDomain: "fbapp-ed8c2.firebaseapp.com",
+	projectId: "fbapp-ed8c2",
+	storageBucket: "fbapp-ed8c2.appspot.com",
+	messagingSenderId: "789876951200",
+	appId: "1:789876951200:web:b49365a44afa2e500fe1c5"
+  };
+  
---
+++ screens/HomeScreen.js
@@ -0,0 +1,25 @@
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+
+export function HomeScreen( props ){
+	const navigation = useNavigation()
+
+	const pressHandler = () => {
+		navigation.navigate("Register")
+	}
+	return (
+		<View>
+			<Text>Home</Text>
+			<TouchableOpacity style={styles.button} onPress={() => pressHandler()}>
+				<Text>Go to Register</Text>
+			</TouchableOpacity>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	button: {
+		backgroundColor: "lightblue",
+		padding: 10,
+	}
+})
